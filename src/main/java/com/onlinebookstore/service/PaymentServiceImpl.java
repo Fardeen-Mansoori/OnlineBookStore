4780c49b63449b57e82e1289d5632307bfa0436b
Devesh2525149 <108398820+Devesh2525149@users.noreply.github.com>
2022-08-14 09:19:21 +0530

    Update PaymentServiceImpl.java

--- src/main/java/com/onlinebookstore/service/PaymentServiceImpl.java
+++ src/main/java/com/onlinebookstore/service/PaymentServiceImpl.java
@@ -40,7 +40,7 @@ public class PaymentServiceImpl implements PaymentService {
 		Optional<Payment> foundPayment = paymentRepository.findById(paymentId);
 
 		if (foundPayment.isEmpty()) {
-			throw new PaymentException("Payment doesn't exists for Id :" + paymentId);
+			throw new PaymentException("Payment doesn't exists for Id : " + paymentId);
 
 		}
 
