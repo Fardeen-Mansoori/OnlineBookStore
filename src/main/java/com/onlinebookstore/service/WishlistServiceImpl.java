b7cfb9ed1c0df1c4de6a9f7b4e6e8b9ac9bf0ec7
kewal2525162 <110464495+kewal2525162@users.noreply.github.com>
2022-08-23 11:13:22 +0530

    wishlist documentation

--- src/main/java/com/onlinebookstore/service/WishlistServiceImpl.java
+++ src/main/java/com/onlinebookstore/service/WishlistServiceImpl.java
@@ -10,6 +10,12 @@ import com.onlinebookstore.dao.WishlistRepository;
 import com.onlinebookstore.dto.Wishlist;
 import com.onlinebookstore.exception.WishlistException;
 
+/************************************************************************************
+ * @author Kewal Shah
+ *  Description It is a service implementation class that provides the services for getting wishlist ,updating wishlist and getting all wishlists. 
+ *  Version 1.0
+ *  Created Date 10/08/2022
+ ************************************************************************************/
 @Service
 public class WishlistServiceImpl implements WishlistService {
 	@Autowired
@@ -35,6 +41,19 @@ public class WishlistServiceImpl implements WishlistService {
 //		return wishlistAdded;
 //	}
 
+	
+	/************************************************************************************
+	 * Method: getWishlistById() Description: To get the wishlist by id
+	 * 
+	 * @param wishlistId
+	 *
+	 * @returns Wishlist- It returns wishlist object, if any error occurs it will
+	 *          throw WishlistException
+	 * @throws WishlistException - It is raised due to invalid wishlistId 
+	 * By - Kewal Shah 
+	 * Created Date - 10-Aug-2022
+	 * 
+	 ************************************************************************************/
 	@Override
 	public Wishlist getWishlistById(Integer wishlistId) throws WishlistException {
 		if (wishlistId == null) {
@@ -47,6 +66,19 @@ public class WishlistServiceImpl implements WishlistService {
 		return foundWishlist.get();
 	}
 
+	
+	/************************************************************************************
+	 * Method: updateWishlist() Description: To update the wishlist
+	 * 
+	 * @param wishlist
+	 *
+	 * @returns Wishlist- It returns wishlist object, if any error occurs it will
+	 *          throw WishlistException
+	 * @throws WishlistException - It is raised due to invalid wishlistId 
+	 * By - Kewal Shah
+	 *  Created Date - 10-Aug-2022
+	 * 
+	 ************************************************************************************/
 	@Override
 	public Wishlist updateWishlist(Wishlist wishlist) throws WishlistException {
 		if (wishlist == null) {
@@ -79,6 +111,19 @@ public class WishlistServiceImpl implements WishlistService {
 //
 //	}
 
+	
+	/************************************************************************************
+	 * Method: getAllWishlist() Description: To get all the wishlists
+	 * 
+	 * @returns WishlistList- It will returns wishlistList, if any error occurs it
+	 *          will throw WishlistException
+	 * @throws WishlistException - If wishlist will be empty it throws no wishlist
+	 *                           found message 
+	 *                           
+	 * By - Kewal Shah 
+	 * Created Date - 10-Aug-2022
+	 * 
+	 ************************************************************************************/
 	@Override
 	public List<Wishlist> getAllWishlist() throws WishlistException {
 		List<Wishlist> wishlistList = this.wishlistRepository.findAll();
