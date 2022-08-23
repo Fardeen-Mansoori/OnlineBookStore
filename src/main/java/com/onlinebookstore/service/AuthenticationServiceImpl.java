030e832644ee0eba7ae7571d23c96aa54aff48f9
Devesh2525149 <108398820+Devesh2525149@users.noreply.github.com>
2022-08-23 10:56:53 +0530

    Update  Authentatication Document

--- src/main/java/com/onlinebookstore/service/AuthenticationServiceImpl.java
+++ src/main/java/com/onlinebookstore/service/AuthenticationServiceImpl.java
@@ -1,12 +1,18 @@
 package com.onlinebookstore.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 import com.onlinebookstore.dto.Authentication;
 import com.onlinebookstore.exception.AdminException;
 import com.onlinebookstore.exception.UserException;
-
+/************************************************************************************
+ * @author Devesh Chitlangia
+ * @Description It is a service class that provides the services for creating a
+ *              new user, get user by ID, Update User and Get All Users. Version
+ *              1.0 Created Date 16-AUG-2022
+ ************************************************************************************/
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
 
@@ -14,6 +20,15 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 	private AdminService adminService;
 	@Autowired
 	UserService userService;
+	/************************************************************************************
+	 * Method: - Add User Description: - Registered User in the Book Store
+	 * Application.
+	 * 
+	 * @object User - User detail
+	 * @returns the saved entity, if user registered otherwise throws UserException
+	 *          Exception
+	 * @throws UserException - It is raised due to User details are invalid or null     
+	 ************************************************************************************/
 	
 	@Override
 	public Boolean adminlogin(Authentication authentication) throws AdminException {
@@ -22,6 +37,15 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 		this.adminService.login(adminId, adminPassword);
 		return true;
 	}
+	/************************************************************************************
+	 * Method: - Add User Description: - Registered User in the Book Store
+	 * Application.
+	 * 
+	 * @object User - User detail
+	 * @returns the saved entity, if user registered otherwise throws UserException
+	 *          Exception
+	 * @throws UserException - It is raised due to User details are invalid or null     
+	 ************************************************************************************/
 
 	@Override
 	public Boolean userLogin(Authentication authentication) throws UserException {
